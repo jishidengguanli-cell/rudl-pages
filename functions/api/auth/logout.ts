8a377b332ed03b066295e34e79806e53ed993364
jishidengguanli-cell <jishidengguanli@gmail.com>
2025-09-15 17:03:32 +0800

    Update logout.ts

--- functions/api/auth/logout.ts
+++ functions/api/auth/logout.ts
@@ -3,6 +3,6 @@ import { clearSessionCookie, Env } from "../_lib/auth";
 export const onRequestPost: PagesFunction<Env> = async () => {
   return new Response("", {
     status: 204,
-    headers: { "set-cookie": clearSessionCookie() },
+    headers: { "set-cookie": clearSessionCookie(), "cache-control": "no-store" },
   });
 };
